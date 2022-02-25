1f6b359f9f2ca70a28a20714f679fe6599618b56
ceyda <54938901+ceydatekin@users.noreply.github.com>
2022-02-25 16:19:53 +0300

    v1.1.2

---
+++ Repository/IRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace CompanyAgreement.Repository
+{
+    public abstract class IRepository<T> where T : class
+    {
+
+       // Models.CompanyContractContext context = ContextManager.GetContext();
+
+
+        //public T GetById(Expression<Func<T, bool>> predicate)
+        //{
+        //    return  context.Set<T>().SingleOrDefault(predicate);
+        //}
+        //public IEnumerable<T> GetAll()
+        //{
+        //    return context.Set<T>().ToList();
+        //}
+
+        //public T Insert(T entity)
+        //{
+
+        //    context.Set<T>().Add(entity);
+        //    Save();
+        //    return entity;
+
+        //}
+        //public void Update(T entity)
+        //{
+
+        //}
+        //public void Delete(T entity)
+        //{
+        //    context.Set<T>().Remove(entity);
+        //    Save();
+        //}
+        //public void Save()
+        //{
+        //    context.SaveChanges();
+        //    //Microsoft.Data.SqlClient.SqlException
+
+
+
+        //}
+
+    }
+}
