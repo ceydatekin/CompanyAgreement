6b38a716e66cfeb2d9bd9f32ff9c7b5bdc76da76
ceyda <54938901+ceydatekin@users.noreply.github.com>
2022-03-11 14:17:23 +0300

    controllerlar boş oluşturuldu

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyAgreement.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyAgreement.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
