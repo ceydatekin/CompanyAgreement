b71408979261879f693c71307ef0ab6e060d0ab2
ceyda <54938901+ceydatekin@users.noreply.github.com>
2022-03-20 22:30:28 +0300

    databasede değişiklik olması gerek devam edilecek

--- Manager/CompanyDepartmantManager.cs
+++ Manager/CompanyDepartmantManager.cs
@@ -11,5 +11,24 @@ namespace CompanyAgreement.Manager
     {
         Context contextManager = ContextManager.GetContext();
         public List<CompanyDepartment> AllCompaniesDepartment() => this.contextManager.CompanyDepartments.ToList();
+        public CompanyDepartment GetQuota(int CompanyId)
+        {
+           
+            return contextManager.CompanyDepartments.SingleOrDefault(s => s.CompanyId == CompanyId);
+        }
+        public void UpdateQuota(int CompanyId)
+        {
+            CompanyDepartment companyDepartment = new CompanyDepartment();
+            companyDepartment = GetQuota(CompanyId);
+            // companyDepartment.DepartmentId == 1;
+
+            switch (companyDepartment.DepartmentId)
+            {
+                case 1: 
+                default:
+                    break;
+            }
+        }
+
     }
 }
